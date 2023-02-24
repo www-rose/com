50256ec8c7f4adf6d1ef3993fed69db6dd42c074
www-rose <126273932+www-rose@users.noreply.github.com>
2023-02-24 08:23:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Mobile App Developer", "Web Developer", "Frontend and Backend Developer"],
+typeSpeed: 100,
+backSpeed:100,
+backDelay: 1000,
+loop: true
+})
